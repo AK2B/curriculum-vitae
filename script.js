fa569c0db63e1560a9149fead7c7bcc1ae665103
AK2B <kennynoar@hotmail.fr>
2022-04-16 13:39:51 +0200

    maj css

--- script.js
+++ script.js
@@ -8,14 +8,11 @@ if (document.readyState === "complete") {
 
 function naissance() {
     var today = new Date();
-    console.log(today);
     var birthDate = new Date("11 25 1987");
-    console.log(birthDate);
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
         age--;
-        console.log(age);
     }
     return (document.getElementById("naissance").innerHTML = age + " ans");
 }
